a77802328bf130f09d2fcac90077c4276d7e0111
Belurgas <70036882+belurgas@users.noreply.github.com>
2025-06-06 18:49:44 +0300

    Add default config create

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,17 @@
+use std::{path::Path, process::exit};
+
 use tokio::fs;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Config {
     pub main_config: MainConfig,
     pub bot_settings: BotSettings,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct MainConfig {
     pub app_id: i32,
     pub api_hash: String,
@@ -17,24 +21,38 @@ pub struct MainConfig {
     pub users: Vec<User>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct User {
     user_id: i64,
     username: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct BotSettings {
     pub target_channel: String,
     pub source_channels: Vec<String>,
 }
 
 impl Config {
-    pub async fn load_config() -> Self {
+    pub async fn load_config() -> Result<Self> {
         let path = "config.json";
+
+        if !Path::new(path).exists() {
+            // Создаём default конфиг
+            let default_config = Config::default();
+
+            let json_str = serde_json::to_string_pretty(&default_config)?;
+
+            fs::write(path, json_str).await?;
+            println!("Создан новый конфиг-файл. Замените соответсвующие поля на ваши");
+            exit(0);
+        }
+
+        // Если файл существует
         let config_str = fs::read_to_string(path).await.expect("Не удалось загрузить конфиг, проверте наличие config.json");
         let config = serde_json::from_str(&config_str).expect("Не удалость спарсить конфиг");
 
-        config
+        Ok(config)
     }
 }
+
