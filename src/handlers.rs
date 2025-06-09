39eff54143d90495476edef9f52e2ddf6fcb29f9
belurgas <tenpart@mail.ru>
2025-06-09 15:14:37 +0300

    Add generate func

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,9 @@
-use std::{collections::HashMap, sync::Arc, time::Duration};
+use std::{collections::HashMap, error::Error, sync::Arc, time::Duration};
 
 use grammers_client::types::Message;
 use tokio::{sync::Mutex, time::Instant};
+
+use crate::mistral::{MistralClient, MistralResponse};
 
 /// Структура для обработки Media
 #[derive(Debug, Clone)]
@@ -55,5 +57,43 @@ impl MediaGroupHandler {
         });
 
         expired
+    }
+}
+
+pub async fn generate(text: &str) -> Result<MistralResponse, Box<dyn Error>> {
+    let api_url = "https://api.mistral.ai/v1/chat/completions".to_string();
+    let model = "pixtral-large-latest".to_string();
+    let client = MistralClient::new(&api_url);
+
+    let system_prompt = r#"
+        Ты — помощник по программированию, который помогает разработчикам обрабатывать текстовые сообщения и фильтровать их на основе заданных критериев. Тебе нужно анализировать входной текст и выдавать структурированный ответ, который можно использовать в коде.
+
+1. Если текст содержит ключевые слова из заданного списка или имеет схожую тематику по словам, отметь его как "релевантный".
+2. Если текст содержит рекламу или ссылки, отметь его как "реклама".
+3. Если текст не соответствует ни одному из критериев, отметь его как "не релевантный".
+4. Если текст похож на новость, то отметь его как релевантный.
+5. Если текст слишком длинный, более 1000 символов, то сделай выжимку из текста, более сжатым и понятным языком вместо оригинального текст, также используй эмодзи.
+Список: ИИ, Нейросети, утилита, утилиты, сервис, новости, новость, модель, OpenAI, Google, Mistral, Gemini, ChatGPT, GPT, DeepSeek, Grok, Elon Mask, технологии
+Формат ответа:
+{
+    "status": "релевантный" | "реклама" | "не релевантный",
+    "text": "оригинальный текст"
+}
+
+ВАЖНО: Ответ должен начинаться с "{" заканчиваться на "}"
+
+Пример входного текста: "Скидка 50% на все товары! Посетите наш сайт: http://example.com"
+    "#;
+    let temperature = 0.7;
+
+    match client.get_response(&model, temperature, text, system_prompt).await {
+        Ok(res) => {
+            // println!("Status: {:#?}", res.choices.iter().map(|ms| ms.message.content.clone()).collect::<Vec<String>>())
+            return Ok(res);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            return Err(e);
+        }
     }
 }
